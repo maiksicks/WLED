37f01f4a3a8c817bd8316b3463a978d575833958
maiksicks <maiksicks@gmail.com>
2024-09-15 19:05:34 +0200

    use ETHClass2 if esp32-s3 is used

--- wled00/wled.h
+++ wled00/wled.h
@@ -81,7 +81,12 @@
 #else // ESP32
   #include <HardwareSerial.h>  // ensure we have the correct "Serial" on new MCUs (depends on ARDUINO_USB_MODE and ARDUINO_USB_CDC_ON_BOOT)
   #include <WiFi.h>
-  #include <ETH.h>
+  #if defined (CONFIG_IDF_TARGET_ESP32S3) && defined (WLED_USE_ETHERNET)
+    #include <ETHClass2.h>
+    #define ETH ETH2
+  #else // ESP32
+    #include <ETH.h>
+  #endif 
   #include "esp_wifi.h"
   #include <ESPmDNS.h>
   #include <AsyncTCP.h>
